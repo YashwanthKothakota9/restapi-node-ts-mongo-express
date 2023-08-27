f3596360fc17155c074a8fe72e8f153dbf6f837e
Yashwanth Kothakota <yashwanthkothakota@gmail.com>
2023-08-27 18:12:25 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ server.listen(8080,()=>{
     console.log('Server running on http://localhost:8080')
 })
 
-const MONGO_URL = 'mongodb+srv://Yashwanth:Yash9mongodb@cluster0.vrwzofi.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URL = ''
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL)
